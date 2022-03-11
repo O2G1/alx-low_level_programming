9308f9d66afb83ff3d7174e71851aacb6d8383ad
O2G1 <geesofttechnology@gmail.com>
2022-03-11 18:51:26 +0000

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,19 @@
 #include <stdio.h>
 
 /**
-* main - Prints a random number and states whether it is positive, negative or zero.
- *
- * Return: Always 0.
- */
-int main (void)
+* main - Prints a random number and states whether
+* it is positive, negative or zero.
+*
+* Return: Always 0.
+*/
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	       printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
