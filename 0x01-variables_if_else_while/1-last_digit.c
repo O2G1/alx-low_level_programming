8bd4f8c68264b1e2ba54ed3f184ea4d749878fd8
O2G1 <geesofttechnology@gmail.com>
2022-03-11 21:29:50 +0000

    alx-low_level_programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
 
-	if (m > 5 )
+	if (m > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
@@ -25,7 +25,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and 0\n", n, m);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
