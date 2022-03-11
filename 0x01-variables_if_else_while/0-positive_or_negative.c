b38e150555a85d4068d2802d868416c74456c294
O2G1 <geesofttechnology@gmail.com>
2022-03-11 14:08:48 +0000

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,11 @@
 
 /*betty style doc for funtcion main goes there*/
 int main (void)
-{
+ {
 	int n;
 
 	srand(time(0));
-	n=rand() -RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return(0);
-}
+ }
