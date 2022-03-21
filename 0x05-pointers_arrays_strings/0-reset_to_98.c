f2d22a0f2a2c3164774cb08c5ae0e61527451ba0
O2G1 <geesofttechnology@gmail.com>
2022-03-21 12:38:20 +0000

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
+* reset_to_98 - updates the value of n to 98
+* @n: integer pointer to a variable to be updated
+*
+* Return: nothing
+*/
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
