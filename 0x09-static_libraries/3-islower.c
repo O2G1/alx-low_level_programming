4d45c78c50dd618a81bd9bfbbc0371a15dbb9853
o2g1 <o2g.firm@gmail.com>
2022-04-01 19:20:09 +0000

    alx-low_level_programming

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,18 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _islower - check the c is lowercase
- * @c: the character to be checked
- * Return: 1 if lowercase and 0 otherwise
+ * _islower - a function that checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
