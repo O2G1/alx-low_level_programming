726d6b86ec1febaedc107ec0e9844325bb4de924
o2g1 <o2g.firm@gmail.com>
2022-04-01 23:28:42 +0000

    alx-low_level_language

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,24 +1,22 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- *_strncat - concatenates two strings
- *@dest: dest
- *@src: src
- *@n: n
- *Return: pointer to dest
- **/
-char *_strncat(char *dest, char *src, int n)
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
 {
-int l, i;
-for (l = 0; dest[l] != '\0'; l++)
-{
-}
-for (i = 0; src[i] != '\0'; i++)
-{
-}
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-dest[l + i] = src[i];
-}
-return (dest);
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
 }
