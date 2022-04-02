e7724b9f71f5dcac88c8a6d73366fde5384fc4ae
o2g1 <o2g.firm@gmail.com>
2022-04-02 13:18:50 +0000

    alx-low_level_programming

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal is to be a competent software engineer\"\n\t- Lawal Olugbeng");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
     return (0);
 }
