93835a41181b30a705492310854c0eb21208b09c
o2g1 <o2g.firm@gmail.com>
2022-04-02 02:10:40 +0000

    alx-low_level_programming

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,16 +1,19 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- *_strlen - return lenght of string
- *@s: pointer s
- *Return: i
- **/
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
 int _strlen(char *s)
 {
-int i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-return (i);
+	int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
 }
