d2668a5f5c010aa23c10a2da33453899cfe656eb
o2g1 <o2g.firm@gmail.com>
2022-04-02 02:07:16 +0000

    alx-low_level_programming

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,16 +1,21 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- *_strchr - locates a character in a string
- *@s: s
- *@c: c
- *Return: pointer
- **/
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++);
-	return (NULL);
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
 	}
+
+	return (NULL);
+}
