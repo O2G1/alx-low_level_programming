2f3ff95c1270464b47b34500db497f6a026b69ce
o2g1 <o2g.firm@gmail.com>
2022-04-21 18:35:30 +0000

    alx-low_level_programming

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,29 @@
 #include "lists.h"
-/**
-  *_strlen - finds length of a string.
-  *@str: string.
-  *
-  *Return: lengt of string.
-  */
-int _strlen(const char *str)
-{
-	int i;
-
-	while (str[i] != '\0')
-		i++;
-
-	return (i);
-}
 
 /**
-  *add_node - adds a new node at the beginning of a list_t.
-  *@head: pointer to first element of the list.
-  *@str: string to be duplicated.
-  *
-  *Return: address of the new element or NULL if it failed.
+  * add_node - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * @str: is a string
+  * Return: a address
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *temp;
+	list_t *new;
+	size_t n;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	/* temp now points to first element*/
-	temp->next = *head;
-	temp->str = strdup(str);
-	temp->len = _strlen(str);
-
-	*head = temp;
-
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (n = 0; new->str[n] != '\0'; n++)
+		;
+	new->len = n;
+	new->next = *head;
+	*head = new;
 	return (*head);
-
 }
